c6086801e859d455750df4e1ef11083c53e630f3
Devika18 <devikankarpe@gmail.com>
2022-10-06 21:49:09 +0530

    added condition for employee present or absent

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -4,6 +4,13 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage program");
+        Random random = new Random();
+        int employeeCheck = random.nextInt(3);
+        if(employeeCheck >=2) {
+            System.out.println("Employee is Present");
+        }
+        else
+            System.out.println("Employee is Absent");
     }
 
 }
