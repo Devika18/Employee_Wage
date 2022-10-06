8813239b7db76cc18b324909f6f1505cb6321770
Devika18 <devikankarpe@gmail.com>
2022-10-06 22:11:45 +0530

    added: logic for calculating part time employee salary

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -6,12 +6,18 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage program");
         int wagePerHour = 20;
         int fullTime = 8;
+        int partTime = 4;
         int salary = 0;
         Random random = new Random();
         int employeeCheck = random.nextInt(3);
         if(employeeCheck >= 2) {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present for Full Time");
             salary = wagePerHour * fullTime;
+            System.out.println("Salary is: "+salary);
+        }
+        else if(employeeCheck >= 1) {
+            System.out.println("Employee is Present for Part Time");
+            salary = wagePerHour * partTime;
             System.out.println("Salary is: "+salary);
         }
         else
