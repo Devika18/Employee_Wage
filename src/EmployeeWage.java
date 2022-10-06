71a2492f4ed68268d0d433faac25b7f061b42501
Devika18 <devikankarpe@gmail.com>
2022-10-06 22:31:30 +0530

    added: logic for calculating monthly salary of the employee

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -7,30 +7,37 @@ public class EmployeeWage {
         int wagePerHour = 20;
         int fullTime = 8;
         int partTime = 4;
-        int salary = 0;
+        int workingDays = 20;
+        int salary;
+        int totalSalary = 0;
         Random random = new Random();
         int employeeCheck = random.nextInt(3);
-        if (employeeCheck == 2) {
-            System.out.println("Employee is Present for Full Time");
-            salary = wagePerHour * fullTime;
-        } else if (employeeCheck == 1) {
-            System.out.println("Employee is Present for Part Time");
-            salary = wagePerHour * partTime;
-        } else {
-            System.out.println("Employee is Absent");
-            switch (employeeCheck) {
-                case 2:
-                    salary = fullTime * wagePerHour;
-                    System.out.println("Employee is present full time");
-                    break;
-                case 1:
-                    salary = partTime * wagePerHour;
-                    System.out.println("Employee is present part time");
-                    break;
-                default:
-                    System.out.println("Employee is absent");
+        for (int day = 0; day <= workingDays; day++) {
+            if (employeeCheck == 2) {
+                System.out.println("Employee is Present for Full Time");
+                salary = wagePerHour * fullTime;
+                totalSalary = totalSalary + salary;
+                System.out.println("Monthly salary is: "+totalSalary);
             }
+            else if (employeeCheck == 1) {
+                System.out.println("Employee is Present for Part Time");
+                salary = wagePerHour * partTime;
+                totalSalary = totalSalary + salary;
+                System.out.println("Monthly salary is: "+totalSalary);
+            } else
+                System.out.println("Employee is Absent");
         }
-
+        switch (employeeCheck) {
+            case 2:
+                salary = fullTime * wagePerHour;
+                System.out.println("Employee is present full time");
+                break;
+            case 1:
+                salary = partTime * wagePerHour;
+                System.out.println("Employee is present part time");
+                break;
+            default:
+                System.out.println("Employee is absent");
+        }
     }
 }
