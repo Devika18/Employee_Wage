c2a9c95ea71cb6281b3c69122579d17235fac418
Devika18 <devikankarpe@gmail.com>
2022-10-06 22:04:50 +0530

    added: logic to calculate daily wage of employee

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -4,10 +4,15 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage program");
+        int wagePerHour = 20;
+        int fullTime = 8;
+        int salary = 0;
         Random random = new Random();
         int employeeCheck = random.nextInt(3);
-        if(employeeCheck >=2) {
+        if(employeeCheck >= 2) {
             System.out.println("Employee is Present");
+            salary = wagePerHour * fullTime;
+            System.out.println("Salary is: "+salary);
         }
         else
             System.out.println("Employee is Absent");
